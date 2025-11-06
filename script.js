faab20128ec196ee1a0b4ef1032d446549bdc2b6
Bellirofont <108865310+Bellirofont@users.noreply.github.com>
2025-11-06 14:46:36 +0300

    Update script.js
    Signed-off-by: Bellirofont <108865310+Bellirofont@users.noreply.github.com>

--- script.js
+++ script.js
@@ -157,17 +157,6 @@ function initMap() {
   createZoneToggleControl();
 }
 
-function setInteractiveRecursive(layer, interactive) {
-  if (layer instanceof L.LayerGroup) {
-    layer.eachLayer(subLayer => setInteractiveRecursive(subLayer, interactive));
-  } else {
-    layer.options.interactive = interactive;
-    if (layer._path) {
-      layer._path.style.pointerEvents = interactive ? 'auto' : 'none';
-    }
-  }
-}
-
 function loadZones() {
   fetch('Fly_Zones_BY.geojson')
     .then(res => {
@@ -202,9 +191,9 @@ function loadZones() {
                 l.bindPopup(`<b>${n}</b><br>${desc}`);
               }
             },
-            style: getZoneStyle
+            style: getZoneStyle,
+            interactive: !isLargeZone
           });
-          setInteractiveRecursive(layer, !isLargeZone);
           zoneLayers[prefixFound].addLayer(layer);
         } else {
           console.warn('Не распознана зона:', name);
